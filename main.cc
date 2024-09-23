4a8363e0cb1f6b3a6802d5be067a22985509dca9
Pau Fernández <pau.fernandez@upc.edu>
2024-09-23 12:13:59 +0200

    Pruebas

--- main.cc
+++ main.cc
@@ -1,14 +1,26 @@
 // main.c
 #include "fenster.h"
 
-const int W = 1024;
-const int H = 768;
+const int width = 1024;
+const int height = 768;
 
-int main()
-{
-    Fenster f(W, H, "fenster");
-    while (f.loop())
-    {
+void clear(Fenster& f) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            f.px(x, y) = 0;
+        }
+    }
+}
+
+int main() {
+    Fenster f(1024, 768, "fenster");
+    while (f.loop(600)) {
         f.px(f.x(), f.y()) = 0xff0000;
+        if (f.key(' ')) {
+            clear(f);
+        }
+        if (f.key(10)) {
+            break;
+        }
     }
 }
